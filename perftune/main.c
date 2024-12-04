cbf983dc37e6b1c574581bf0d0b7d93dde626938
231098166 <231098166@smail.nju.edu.cn>
2024-12-04 13:59:29 +0800

    QWe

--- perftune/main.c
+++ perftune/main.c
@@ -1,12 +1,17 @@
-#include <stdint.h>
 #include <stdio.h>
+
+#define N 10000000
 
 int *sieve(int n);
 
-int main() {
-  int *primes = sieve(400);
-  for (int *cur = primes, i = 0; *cur; cur++, i++) {
-    printf("%8d", *cur);
-    if (i % 8 == 7 || !*(cur + 1)) printf("\n");
+int main()
+{
+  int *primes = sieve(N - 1);
+  int count = 0;
+  for (int *cur = primes; *cur; cur++)
+  {
+    count++;
   }
+  // printf("素数总数：%d\n", count);
+  return 0;
 }
--- perftune/sieve.c
+++ perftune/sieve.c
@@ -1,29 +1,41 @@
-#include <stdbool.h>
-#include <string.h>
-#include <assert.h>
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
 #define N 10000000
 
-static bool is_prime[N];
-static int  primes[N];
+static unsigned char is_prime[(N + 7) / 8];
+static int primes[N];
 
-int *sieve(int n) {
-  assert(n + 1 < N);
-  for (int i = 0; i <= n; i++)
-    is_prime[i] = true;
+#define IS_PRIME(n) (is_prime[(n) / 8] & (1 << ((n) % 8)))
+#define SET_COMPOSITE(n) (is_prime[(n) / 8] &= ~(1 << ((n) % 8)))
 
-  for (int i = 2; i <= n; i++) {
-    for (int j = i + i; j <= n; j += i) {
-      is_prime[j] = false;
+int *sieve(int n)
+{
+  memset(is_prime, 0xFF, sizeof(is_prime));
+  SET_COMPOSITE(0);
+  SET_COMPOSITE(1);
+  int sqrt_n = (int)sqrt(n);
+
+  for (int i = 2; i <= sqrt_n; i++)
+  {
+    if (IS_PRIME(i))
+    {
+      for (int j = i * i; j <= n; j += i)
+      {
+        SET_COMPOSITE(j);
+      }
     }
   }
 
   int *p = primes;
   for (int i = 2; i <= n; i++)
-    if (is_prime[i]) {
+  {
+    if (IS_PRIME(i))
+    {
       *p++ = i;
     }
+  }
   *p = 0;
   return primes;
 }
